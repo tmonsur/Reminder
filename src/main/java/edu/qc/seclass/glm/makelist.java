49cce4489a7f8bae6b6cff5857d674199e14c209
tmonsur000 <tmonsur000@gmail.com>
2019-11-05 20:15:35 -0500

    added necesarry files

--- src/main/java/edu/qc/seclass/glm/makelist.java
+++ src/main/java/edu/qc/seclass/glm/makelist.java
@@ -2,7 +2,9 @@ package edu.qc.seclass.glm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,11 +13,25 @@ public class makelist extends AppCompatActivity {
 
     private TextView textView;
     private EditText editText;
+    private Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_makelist);
         textView=(TextView) findViewById(R.id.textView);
         editText=(EditText) findViewById(R.id.editText);
+        button3=(Button) findViewById(R.id.button3);
+
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
